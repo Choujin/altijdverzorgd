2e8a1501e7cc78b974ff425ba58c6417cd004192
Richard Alp <recho.alp@gmail.com>
2019-02-08 13:27:36 +0100

    home page test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    AV Business
+                    AV Business!
                 </div>
 
                 <div class="links">
