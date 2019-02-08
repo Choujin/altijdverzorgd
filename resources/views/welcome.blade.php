e5be6abb8982e91dd0804051cd54bcbf837c1f4b
Richard Alp <recho.alp@gmail.com>
2019-02-08 13:53:33 +0100

    homepage test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    AV Business!
+                    Altijd verzorgd MKB
                 </div>
 
                 <div class="links">
