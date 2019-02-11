fabf88a6c6718ead792ad57ba28477ea366d4e09
Richard Alp <recho.alp@gmail.com>
2019-02-11 16:32:27 +0100

    business first section added + navbar fix padding top

--- resources/views/business-info.blade.php
+++ resources/views/business-info.blade.php
@@ -2,8 +2,17 @@
 @section('title','Altijd verzorgd | jouw business partner')
 <body id="app">
 @include('components.headers.hpheader')
-
-
+<section class="bg-green">
+    <div class="container text-center">
+            <h1 class="text-white display-3">Meer klanten, hogere omzet <br> en minder no-shows...</h1>
+            <br>
+            <h2 class="text-white display-4">Interesse?</h2>
+            <br>
+            <button type="button" class="btn btn-light btn-lg auto">Vraag nu demo aan</button>
+            <br>
+            <br>
+    </div>
+    </section>
 
 @include('components.footer')
 @include('components.scripts')
