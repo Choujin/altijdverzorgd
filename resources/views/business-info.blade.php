56ff3471edbe99fe6bc7d3c447a84c5b15f1d594
Richard Alp <recho.alp@gmail.com>
2019-02-12 16:11:53 +0100

    form added

--- resources/views/business-info.blade.php
+++ resources/views/business-info.blade.php
@@ -78,7 +78,132 @@
         <div class="container">
           <h1 class="color-green display-5 pt-4"> Vul hieronder het <br> aanmeldingsformulier in</h1>
           <p class="display-6 py-4 text-dark">Vul vrijblijvend het aanmeldingsformulier in zodat wij telefonisch contact met je kunnen opnemen.</p>
-         
+          
+          <form class="form-signin bg-white rounded" method="POST" action="{{ route('member.register') }}" aria-label="{{ __('Register') }}">
+              @csrf
+              
+              <div class="form-group row ">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Voornaam') }}</label>
+        
+                  <div class="col-3">
+                      <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+        
+                      @if ($errors->has('name'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('name') }}</strong>
+                          </span>
+                      @endif
+                  </div>
+              </div>
+              <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Achternaam') }}</label>
+        
+                  <div class="col-3">
+                      <input id="surname" type="text" class="form-control{{ $errors->has('surname') ? ' is-invalid' : '' }}" name="surname" value="{{ old('surname') }}" required autofocus>
+        
+                      @if ($errors->has('surname'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('surname') }}</strong>
+                          </span>
+                      @endif
+                  </div>
+              </div>
+        
+              <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Geslacht') }}</label>
+        
+                  <div class="col-3 row d-flex justify-content-around ml-0">
+                      <div class="custom-control custom-radio">
+                        <input id="customRadio1" type="radio" name="gender" value="men" class="custom-control-input">
+                        <label class="custom-control-label" for="customRadio1">Man</label>
+                      </div>
+                      <div class="custom-control custom-radio">
+                        <input id="customRadio2" type="radio" name="gender" value="women" class="custom-control-input">
+                        <label class="custom-control-label" for="customRadio2">Vrouw</label>
+                      </div>
+                      @if ($errors->has('surname'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('surname') }}</strong>
+                          </span>
+                      @endif
+                  </div>
+              </div>
+        
+              <div class="form-group row">
+                  <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        
+                  <div class="col-3">
+                      <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+        
+                      @if ($errors->has('email'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('email') }}</strong>
+                          </span>
+                      @endif
+                  </div>
+              </div>
+        
+              <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+        
+                  <div class="col-3">
+                      <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+        
+                      @if ($errors->has('password'))
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $errors->first('password') }}</strong>
+                          </span>
+                      @endif
+                  </div>
+              </div>
+        
+              <div class="form-group row">
+                  <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+        
+                  <div class="col-3">
+                      <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                  </div>
+              </div>
+              <div class="form-group row">
+                  <label for="work_location" class="col-md-4 col-form-label text-md-right">{{ __('Work location') }}</label>
+        
+                  <div class="col-3">
+                      <input id="work_location" type="text" class="form-control" name="work_location" required>
+                  </div>
+              </div>
+              <div class="form-group row">
+                    <label for="specialisation" class="col-md-4 col-form-label text-md-right">{{ __('Specialisation') }}</label>
+        
+                    <div class="col-3">
+                        <input id="specialisation" type="text" class="form-control" name="specialisation" required>
+                    </div>
+              </div>
+              <!-- <div class="form-group row">
+                  <label for="exampleFormControlSelect1" class="col-md-4 col-form-label text-md-right">{{ __('Specialisation') }}</label>
+                  <select class="form-control col" id="specialisation" required>
+                    <option>Kapper</option>
+                    <option>Massagist</option>
+                    <option>Manicure</option>
+                    <option>Pedicure</option>
+                  </select>
+              </div> -->
+              <div class="form-group row mb-0">
+                  <div class="col-3 offset-md-4">
+                      <button type="submit" class="btn btn-success btn-block">
+                          {{ __('Register') }}
+                      </button>
+                  </div>
+              </div>
+              <a class="btn btn-link" href="{{ route('member.login') }}">
+                  {{ __('Login') }}
+              </a>
+          /
+              <a class="btn btn-link" href="{{ route('member.password.request') }}">
+                  {{ __('Forgot Your Password?') }}
+              </a>
+              
+          </form>
+
         </div>
       </section>
 
