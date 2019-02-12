a041a1cabcd81a01ec97f5bdf89ec14cf1b1048d
Richard Alp <recho.alp@gmail.com>
2019-02-12 15:36:50 +0100

    section 3 done

--- resources/views/business-info.blade.php
+++ resources/views/business-info.blade.php
@@ -4,8 +4,8 @@
 @include('components.headers.hpheader')
     <section class="bg-green pb-5">
         <div class="container text-center">
-            <h1 class="text-white display-3 pt-4">Meer klanten, hogere omzet <br> en minder no-shows...</h1>
-            <h2 class="text-white display-4 py-4">Interesse?</h2>
+            <h1 class="display-3 pt-4">Meer klanten, hogere omzet <br> en minder no-shows...</h1>
+            <h2 class="display-4 py-4">Interesse?</h2>
             <button type="button" class="btn btn-light btn-lg auto my-5s">Vraag nu demo aan</button>
         </div>
     </section>
@@ -25,8 +25,7 @@
               <h4>.. en meer terugkerende klanten</h4>
               <p>Deze klanten zijn bijna als familie</p>
             </div>
-        </div>
-        <div class="row">
+     
           <div class="col-sm-12 col-md-6">
             <img class="p-2" data-src="images/business-info-woman.svg" width="130px" alt="">
             <h4>Verminder het aantal no-shows</h4>
@@ -45,7 +44,47 @@
         </div>
       </div>
     </section>
+
+    <section class="bg-green pb-5">
+        <div class="container text-center">
+            <h1 class="text-white display-5 py-5">Wat kan Altijd verzorgd voor jou betekenen?</h1>
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <img class="img-fluid" data-src="images/business-info-desktop-phone.png" alt="Altijd verzorgd websites">
+                </div>
+                <div class="col-sm-12 col-md-6 text-left mt-5">
+                    <h2>44%</h2>
+                    <p>De gemiddelde groei in boekingen die je kunt <br> verwachten in het eerste jaar.</p>
+                    <h2 class="pt-3">Elke 1.5 seconde</h2>
+                    <p>Maakt een klant een boeking via Treatwell.</p>
+                </div>
+              </div>
+                <div class="row my-5">
+         
+                <div class="col-sm-12 col-md-6 text-left my-5">
+                    <h2 class="pt-3">47%</h2>
+                    <p>Van de boekingen worden buiten openingsuren gemaakt.</p>
+                    <h2 class="pt-3">4.8/5</h2>
+                    <p>De App store score van onze Altijd verzorgd.</p>
+                </div>
+              <div class="col-sm-12 col-md-6">
+                  <img class="img-fluid" data-src="images/business-info-calendar-view.png" alt="Altijd verzorgd websites">
+              </div>      
+          </div>
+        </div>
+    </section>
     
+    <section class="text-center py-5">
+        <div class="container color-green">
+          <h1 class="text-green display-5 pt-4"> Vul hieronder het <br> aanmeldingsformulier in</h1>
+          <p class="text-green display-6 py-4">Vul vrijblijvend het aanmeldingsformulier in zodat wij telefonisch contact met je kunnen opnemen.</p>
+         
+        </div>
+      </section>
+
+
+
+
       
 @include('components.footer')
 @include('components.scripts')
