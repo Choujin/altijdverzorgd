dd54b92ad87fea1006077c219f5ed520b67e67ea
JettazIFrankenstein <xstasx@live.nl>
2019-02-13 13:47:31 +0100

    Merge branch 'master' of https://github.com/jettaz/avbusiness

--- resources/views/contents/homepage/section1.blade.php
+++ resources/views/contents/homepage/section1.blade.php
@@ -6,7 +6,7 @@
           <!-- data-toggle="tooltip" data-placement="top" title="Uw adres" -->
           <button type='button' class="btn btn-success rounded-left" aria-label="uw adres"><i class="fas fa-map-marker" title="uw adres"></i></button>
         </div>
-        <input type="text" id="searchadres" name="address" class="form-control" placeholder="Uw adres, b.v. Van Heekplein 300">
+        <input type="text" id="searchcity" name="city" class="form-control" placeholder="Uw woonplaats, b.v. Enschede" required>
       </label>
       <div class="row no-gutters">
         <div class="col-lg-5 mr-lg-1">
@@ -39,16 +39,16 @@
       </div>
       <div class="btn-group btn-group-toggle col-12 p-0 mt-1 my-lg-0 justify-content-between" data-toggle="buttons">
         <label class="btn btn-success col-lg-3 col-3 rounded" name="keuze" for="voorvrouwen" data-toggle="tooltip" data-placement="top" title="Voor vrouw">
-          <input type="radio" name="keuze" id="voorvrouwen" value="female"><i class="fas fa-female fa-2x" title="Voor vrouw"></i>
+          <input type="radio" name="select" id="voorvrouwen" value="female" required><i class="fas fa-female fa-2x" title="Voor vrouw"></i>
         </label>
         <label class="btn btn-success col-lg-3 col-3 ml-1 mr-1 rounded" name="keuze" for="voormannen" data-toggle="tooltip" data-placement="top" title="Voor man">
-          <input type="radio" name="keuze" id="voormannen" value="male"><i class="fas fa-male fa-2x" title="Voor man"></i>
+          <input type="radio" name="select" id="voormannen" value="male"><i class="fas fa-male fa-2x" title="Voor man"></i>
         </label>
         <label class="btn btn-success col-lg-3 col-3 mr-1 rounded" name="keuze" for="voorkinderen" data-toggle="tooltip" data-placement="top" title="Voor je kind(eren)">
-          <input type="radio" name="keuze" id="voorkinderen" value="kids"><i class="fas fa-child fa-2x" title="Voor je kind(eren)"></i>
+          <input type="radio" name="select" id="voorkinderen" value="kids"><i class="fas fa-child fa-2x" title="Voor je kind(eren)"></i>
         </label>
         <label class="btn btn-success col-lg-3 col-3 rounded" name="keuze" for="voorhuisdieren" data-toggle="tooltip" data-placement="top" title="Voor je huisdier(en)">
-          <input type="radio" name="keuze" id="voorhuisdieren" value="pets"><i class="fas fa-paw fa-2x" title="Voor je huisdier(en)"></i>
+          <input type="radio" name="select" id="voorhuisdieren" value="pets"><i class="fas fa-paw fa-2x" title="Voor je huisdier(en)"></i>
         </label>
       </div>
       <button class="btn btn-success btn-block mt-1" type="submit">Zoek</button>
