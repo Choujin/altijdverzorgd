433dfbd1c301d6a25356b4457a6eea0c792146b4
CodeGorilla <codegorilla@MacBook-Air-van-CodeGorilla.local>
2019-02-19 10:16:34 +0100

    cris

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -1,6 +1,15 @@
 @extends('member.layouts.app')
 
 <style type="text/css">
+@media screen and (max-width: 1200px)
+{
+
+}
+.container{
+  width: 100%;
+  max-width: 1200px;
+}
+
 .profile-image{
   width: 150px;
   margin-top: 20px;
@@ -11,7 +20,7 @@
   background-color: green;
 }
 .ster{
-  width: 30px;
+  width: 20px;
 }
 
 </style>
@@ -19,19 +28,17 @@
 @section('content')
 
 
-<div class="container">
-  <div class="row">
-    <div class="col">
+<div class="container-fluid">
       <div class="row">
-        <div class="col-3">
+        <div class="col-sm-3 text-center">
           <img data-src="/images/avatar/default.jpg" alt="your profile image" class="profile-image rounded-circle p-1">
         </div>
-        <div class="col-5 align-self-end">
+        <div class="col-sm-6 align-self-end">
           <h3>Cristepher</h3>
           <h6>ceadamus@outlook.com</h6>
 
           <hr class="bg-green">
-          <div class="col card border-success mb-3">
+          <div class="col-sm-6 card border-success ">
             <B><p>
               Werk-locatie: Enschede <br>
               Specialicatie: Massage
@@ -41,21 +48,36 @@
 
         </div>
       </div>
-    </div>
-  </div>
 
-</div>
+
 <hr class="bg-green">
 
 <div class="row">
-  <div class="col">
-    <h5>Comments<h5>
+  <div class="col-sm-4">
+    <h5>Agenda<h5>
+
+
       </div>
-      <div class="col">
-<img data-src="/images/avatar/ster.png" class="ster justify-content-between">
+
+<div class="col-sm-4 text-center">
+<img data-src="/images/avatar/ster.png" class="ster"><B>10</B>
 </div>
 
-</div>
+<div class="col-sm-4 text-right">
+  <h5>Comments<h5>
 
+</div>
+</div>
+<div class="row justify-content-between">
+  <div class="col-sm-5">
+    <img data-src="/images/avatar/agenda.jpeg" alt="">
+  </div>
+  <div class="col-sm-5 ">
+  <hr>
+  <p>lorem ipusum</p>
+  <hr>
+</div>
+</div>
+</div>
 
 @endsection
