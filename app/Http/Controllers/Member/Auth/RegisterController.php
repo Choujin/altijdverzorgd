23ed2f71cf5425d6c2c5d98dfad903f4ae0db094
JettazIFrankenstein <xstasx@live.nl>
2019-02-22 09:33:54 +0100

    some fixes

--- app/Http/Controllers/Member/Auth/RegisterController.php
+++ app/Http/Controllers/Member/Auth/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Member\Auth;
 
 use App\Member;
 use App\Http\Controllers\Controller;
-use App\Notifications\UserRegisteredNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
