e1672b654b1d447f8a14f3c5de937926e30eb7dd
Richard Alp <recho.alp@gmail.com>
2019-02-25 15:21:12 +0100

    added new page general conditions

---
+++ resources/views/general-conditions.blade.php
@@ -0,0 +1,36 @@
+@include ('components.head')
+@section('title','Altijd verzorgd | Algemeen voorwaarden')
+
+<body id="app">
+    @include('components.headers.hpheader')
+
+    <section class="business-info-bg pb-5">
+            <div class="container h-100">
+                    <div class="row align-items-center h-100">
+                        <div class="col-12 mx-auto">
+                            <div class="text-center text-white">
+                                    <h1>Algemeen voorwaarden</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+
+    <section class="text-center py-5">
+        <div class="container">
+            <h1 class="display-5 pt-4">Altijd verzorgd website algemene voorwaarden</h1>
+                
+            <h3>Laatst bijgewerkt Februari 2019</h3>
+            
+            <h2 class="text-green display-6 py-4">Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of you time today. <br>Thanks for being you.
+
+             <p><br>Altijd verzorgd.</p></h2>
+            
+        </div>
+    </section>
+
+    @include('components.footer')
+    @include('components.scripts')
+</body>
+
+</html>
